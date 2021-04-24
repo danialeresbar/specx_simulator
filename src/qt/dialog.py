c6cd9dee56eefe68cc53f2c410adffdb16444428
Daniel Restrepo <daniel.restrepo@unillanos.edu.co>
2021-04-24 02:22:25 -0500

    Refactor old source

--- src/qt/dialog.py
+++ src/qt/dialog.py
@@ -2,17 +2,23 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtChart import QChartView
 
 
-# ---- Distribution parameters ----
-FIRST_PARAMETER = 'Parámetro 1:'
-SECOND_PARAMETER = 'Parámetro 2:'
-THIRD_PARAMETER = 'Parámetro 3:'
-FOURTH_PARAMETER = 'Parámetro 4:'
-ONE_PARAMETER = '1P'
-TWO_PARAMETERS = '2P'
-THREE_PARAMETERS = '3P'
+# ---- Dialog attributes ----
+MINIMUM_WIDTH = 800
+MINIMUM_HEIGHT = 400
+DEFAULT_WIDTH = 800
+DEFAULT_HEIGHT = 400
+SIZE_POLICY_EXPANDING = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+SIZE_POLICY_PREFERRED = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
 
+# ---- Labels ----
+DEFAULT_PARAMETER_LABELS = (
+    ('1P', 'Parámetro 1:'),
+    ('2P', 'Parámetro 2:'),
+    ('3P', 'Parámetro 3:'),
+    ('4P', 'Parámetro 4:')
+)
 
-# ---- GUI fonts ----
+# ---- Fonts ----
 BOX_LABEL_FONT = QtGui.QFont()
 BOX_LABEL_FONT.setPointSizeF(12)
 MAIN_LABEL_FONT = QtGui.QFont()
@@ -21,26 +27,26 @@ RADIO_LABEL_FONT = QtGui.QFont()
 RADIO_LABEL_FONT.setPointSizeF(10)
 
 
-class UiConfigDialog(object):
-    def setupUi(self, config_dialog):
-        config_dialog.setObjectName("config_dialog")
-        config_dialog.resize(800, 400)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(config_dialog.sizePolicy().hasHeightForWidth())
-        config_dialog.setSizePolicy(sizePolicy)
-        config_dialog.setMinimumSize(QtCore.QSize(800, 400))
-        config_dialog.setMaximumSize(QtCore.QSize(800, 400))
-        icon = QtGui.QIcon()
-        icon.addPixmap(
+class DialogTemplate:
+    def setup(self, dialog, **kwargs):
+        dialog.setObjectName("dialog")
+        dialog.resize(kwargs.get('width', DEFAULT_WIDTH), kwargs.get('height', DEFAULT_HEIGHT))
+        dialog_size_policy = SIZE_POLICY_PREFERRED
+        dialog_size_policy.setHorizontalStretch(0)
+        dialog_size_policy.setVerticalStretch(0)
+        dialog_size_policy.setHeightForWidth(dialog.sizePolicy().hasHeightForWidth())
+        dialog.setSizePolicy(dialog_size_policy)
+        dialog.setMinimumSize(QtCore.QSize(MINIMUM_WIDTH, MINIMUM_HEIGHT))
+        dialog.setMaximumSize(QtCore.QSize(MINIMUM_WIDTH, MINIMUM_HEIGHT))
+        dialog_icon = QtGui.QIcon()
+        dialog_icon.addPixmap(
             QtGui.QPixmap("../icons/icon.svg"),
             QtGui.QIcon.Normal,
             QtGui.QIcon.Off
         )
-        config_dialog.setWindowIcon(icon)
+        dialog.setWindowIcon(dialog_icon)
 
-        config_dialog.setStyleSheet("* {\n"
+        dialog.setStyleSheet("* {\n"
 "    background: #26282b;\n"
 "    color: #DDDDDD;\n"
 "    border: 1px solid #5A5A5A;\n"
@@ -252,213 +258,171 @@ class UiConfigDialog(object):
 "QMenu::separator {\n"
 "    background: #7971ea;\n"
 "}")
-        config_dialog.setModal(True)
+        dialog.setModal(True)
 
-        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(config_dialog)
-        self.horizontalLayout_3.setContentsMargins(10, 10, 10, 10)
-        self.horizontalLayout_3.setSpacing(5)
-
-        self.chart_preview_box = QtWidgets.QGroupBox(config_dialog)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.chart_preview_box.sizePolicy().hasHeightForWidth())
-        self.chart_preview_box.setSizePolicy(sizePolicy)
-        self.chart_preview_box.setFont(BOX_LABEL_FONT)
-        self.pdf_chartview = QChartView()
-        self.pdf_chartview.setRenderHint(QtGui.QPainter.Antialiasing)
-        self.view_layout = QtWidgets.QHBoxLayout(self.chart_preview_box)
-        self.view_layout.setContentsMargins(10, 15, 10, 10)
-        self.view_layout.setSpacing(5)
-        self.view_layout.addWidget(self.pdf_chartview)
-        self.horizontalLayout_3.addWidget(self.chart_preview_box)
+        self.dialog_layout = QtWidgets.QHBoxLayout(dialog)
+        self.dialog_layout.setContentsMargins(10, 10, 10, 10)
+        self.dialog_layout.setSpacing(5)
 
         self.control_layout = QtWidgets.QVBoxLayout()
         self.control_layout.setContentsMargins(10, 40, 10, 20)
         self.control_layout.setSpacing(30)
 
-        self.parameters_box = QtWidgets.QGroupBox(config_dialog)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.parameters_box.sizePolicy().hasHeightForWidth())
-        self.parameters_box.setSizePolicy(sizePolicy)
-        self.parameters_box.setFont(BOX_LABEL_FONT)
-        self.parameters_box.setObjectName("parameters_box")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.parameters_box)
-        self.verticalLayout.setContentsMargins(10, 20, 10, 10)
-        self.verticalLayout.setSpacing(5)
+        # Preview components
+        self.chart_preview_box = QtWidgets.QGroupBox(dialog)
+        self.chart_preview = QChartView()
+        self._build_preview_box()
 
-        self.row_1 = QtWidgets.QHBoxLayout()
-        self.row_1.setContentsMargins(40, -1, 10, -1)
-        self.row_1.setSpacing(5)
+        # Parameter components
+        self.parameter_box = QtWidgets.QGroupBox(dialog)
+        self.parameter_labels = [QtWidgets.QLabel(self.parameter_box) for _ in range(len(DEFAULT_PARAMETER_LABELS))]
+        self.parameter_spinners = [QtWidgets.QDoubleSpinBox(self.parameter_box) for _ in range(len(DEFAULT_PARAMETER_LABELS))]
+        self._build_parameter_box()
 
-        self.radiobtn_1 = QtWidgets.QRadioButton(self.parameters_box)
-        self.radiobtn_1.setEnabled(True)
-        self.radiobtn_1.setFont(RADIO_LABEL_FONT)
-        self.row_1.addWidget(self.radiobtn_1)
+        # self.row_1 = QtWidgets.QHBoxLayout()
+        # self.row_1.setContentsMargins(40, -1, 10, -1)
+        # self.row_1.setSpacing(5)
+        #
+        # self.radiobtn_1 = QtWidgets.QRadioButton(self.parameters_box)
+        # self.radiobtn_1.setEnabled(True)
+        # self.radiobtn_1.setFont(RADIO_LABEL_FONT)
+        # self.row_1.addWidget(self.radiobtn_1)
+        #
+        # self.radiobtn_2 = QtWidgets.QRadioButton(self.parameters_box)
+        # self.radiobtn_2.setEnabled(True)
+        # self.radiobtn_2.setFont(RADIO_LABEL_FONT)
+        # self.row_1.addWidget(self.radiobtn_2)
+        #
+        # self.radiobtn_3 = QtWidgets.QRadioButton(self.parameters_box)
+        # self.radiobtn_3.setFont(RADIO_LABEL_FONT)
+        # self.row_1.addWidget(self.radiobtn_3)
 
-        self.radiobtn_2 = QtWidgets.QRadioButton(self.parameters_box)
-        self.radiobtn_2.setEnabled(True)
-        self.radiobtn_2.setFont(RADIO_LABEL_FONT)
-        self.row_1.addWidget(self.radiobtn_2)
+        # self.radios = [self.radiobtn_1, self.radiobtn_2, self.radiobtn_3]
 
-        self.radiobtn_3 = QtWidgets.QRadioButton(self.parameters_box)
-        self.radiobtn_3.setFont(RADIO_LABEL_FONT)
-        self.row_1.addWidget(self.radiobtn_3)
+        # self.btn_layout_outer = QtWidgets.QVBoxLayout()
+        # self.btn_layout_outer.setContentsMargins(0, 0, 0, 0)
+        # self.btn_layout_outer.setSpacing(10)
 
-        self.row_2 = QtWidgets.QHBoxLayout()
-        self.row_2.setContentsMargins(5, 5, 5, 5)
-        self.row_2.setSpacing(5)
+        self.btn_submit = QtWidgets.QPushButton(dialog)
+        self.btn_reject = QtWidgets.QPushButton(dialog)
+        self._build_button_box()
 
-        self.parameter_label_1 = QtWidgets.QLabel(self.parameters_box)
-        self.parameter_label_1.setFont(MAIN_LABEL_FONT)
-        self.parameter_spbox_1 = QtWidgets.QDoubleSpinBox(self.parameters_box)
-        self.parameter_spbox_1.setEnabled(True)
-        self.parameter_spbox_1.setFont(MAIN_LABEL_FONT)
-        self.parameter_spbox_1.setDecimals(10)
-        self.parameter_spbox_1.setSingleStep(0.1)
-        self.parameter_spbox_1.setValue(0.5)
+        self.dialog_layout.addLayout(self.control_layout)
+        self.dialog_layout.setStretch(0, 60)
+        self.dialog_layout.setStretch(1, 40)
 
-        self.row_2.addWidget(self.parameter_label_1)
-        self.row_2.addWidget(self.parameter_spbox_1)
-        self.row_2.setStretch(0, 40)
-        self.row_2.setStretch(1, 60)
+        self.retranslateUi(dialog)
+        QtCore.QMetaObject.connectSlotsByName(dialog)
 
-        self.row_3 = QtWidgets.QHBoxLayout()
-        self.row_3.setContentsMargins(5, 5, 5, 5)
-        self.row_3.setSpacing(5)
+    def _build_button_box(self):
+        """
 
-        self.parameter_label_2 = QtWidgets.QLabel(self.parameters_box)
-        self.parameter_label_2.setFont(MAIN_LABEL_FONT)
-        self.parameter_spbox_2 = QtWidgets.QDoubleSpinBox(self.parameters_box)
-        self.parameter_spbox_2.setFont(MAIN_LABEL_FONT)
-        self.parameter_spbox_2.setDecimals(10)
-        self.parameter_spbox_2.setSingleStep(0.1)
-        self.parameter_spbox_2.setObjectName("parameter_spbox_2")
-        self.parameter_spbox_2.setValue(0.2)
+        """
 
-        self.row_3.addWidget(self.parameter_label_2)
-        self.row_3.addWidget(self.parameter_spbox_2)
-        self.row_3.setStretch(0, 40)
-        self.row_3.setStretch(1, 60)
+        # Size policies
+        btn_submit_size_policy = SIZE_POLICY_PREFERRED
+        btn_submit_size_policy.setHorizontalStretch(0)
+        btn_submit_size_policy.setVerticalStretch(0)
+        btn_submit_size_policy.setHeightForWidth(self.btn_submit.sizePolicy().hasHeightForWidth())
+        self.btn_submit.setSizePolicy(btn_submit_size_policy)
+        btn_reject_size_policy = SIZE_POLICY_PREFERRED
+        btn_reject_size_policy.setHorizontalStretch(0)
+        btn_reject_size_policy.setVerticalStretch(0)
+        btn_reject_size_policy.setHeightForWidth(self.btn_reject.sizePolicy().hasHeightForWidth())
+        self.btn_reject.setSizePolicy(btn_reject_size_policy)
 
-        self.row_4 = QtWidgets.QHBoxLayout()
-        self.row_4.setContentsMargins(5, 5, 5, 5)
-        self.row_4.setSpacing(5)
+        # Layout
+        btn_layout = QtWidgets.QHBoxLayout()
+        btn_layout.setContentsMargins(40, 5, 40, 5)
+        btn_layout.setSpacing(0)
 
-        self.parameter_label_3 = QtWidgets.QLabel(self.parameters_box)
-        self.parameter_label_3.setFont(MAIN_LABEL_FONT)
-        self.parameter_label_3.setObjectName("parameter_label_3")
-        self.row_4.addWidget(self.parameter_label_3)
-        self.parameter_spbox_3 = QtWidgets.QDoubleSpinBox(self.parameters_box)
-        self.parameter_spbox_3.setFont(MAIN_LABEL_FONT)
-        self.parameter_spbox_3.setDecimals(10)
-        self.parameter_spbox_3.setSingleStep(0.1)
-        self.parameter_spbox_3.setObjectName("parameter_spbox_3")
-        self.parameter_spbox_3.setValue(0.2)
-
-        self.row_4.addWidget(self.parameter_spbox_3)
-        self.row_4.setStretch(0, 40)
-        self.row_4.setStretch(1, 60)
-
-        self.row_5 = QtWidgets.QHBoxLayout()
-        self.row_5.setContentsMargins(5, 5, 5, 5)
-        self.row_5.setSpacing(5)
-
-        self.parameter_label_4 = QtWidgets.QLabel(self.parameters_box)
-        self.parameter_label_4.setFont(MAIN_LABEL_FONT)
-        self.parameter_label_4.setObjectName("parameter_label_4")
-        self.row_5.addWidget(self.parameter_label_4)
-        self.parameter_spbox_4 = QtWidgets.QDoubleSpinBox(self.parameters_box)
-        self.parameter_spbox_4.setFont(MAIN_LABEL_FONT)
-        self.parameter_spbox_4.setSingleStep(0.1)
-        self.parameter_spbox_4.setDecimals(10)
-        self.parameter_spbox_4.setValue(0.5)
-
-        self.row_5.addWidget(self.parameter_spbox_4)
-        self.row_5.setStretch(0, 40)
-        self.row_5.setStretch(1, 60)
-
-        self.verticalLayout.addLayout(self.row_1)
-        self.verticalLayout.addLayout(self.row_2)
-        self.verticalLayout.addLayout(self.row_3)
-        self.verticalLayout.addLayout(self.row_4)
-        self.verticalLayout.addLayout(self.row_5)
-
-        self.control_layout.addWidget(self.parameters_box)
-
-        self.radios = [self.radiobtn_1, self.radiobtn_2, self.radiobtn_3]
-
-        self.parameter_labels = [self.parameter_label_1, self.parameter_label_2,
-                                 self.parameter_label_3, self.parameter_label_4]
-
-        self.spboxes = [self.parameter_spbox_1, self.parameter_spbox_2,
-                                  self.parameter_spbox_3, self.parameter_spbox_4]
-
-        self.btn_layout_outer = QtWidgets.QVBoxLayout()
-        self.btn_layout_outer.setContentsMargins(0, 0, 0, 0)
-        self.btn_layout_outer.setSpacing(10)
-
-        self.btn_layout_inner = QtWidgets.QHBoxLayout()
-        self.btn_layout_inner.setContentsMargins(40, 5, 40, 5)
-        self.btn_layout_inner.setSpacing(0)
-
-        self.btn_submit = QtWidgets.QPushButton(config_dialog)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.btn_submit.sizePolicy().hasHeightForWidth())
-        self.btn_submit.setSizePolicy(sizePolicy)
+        # Buttons
         self.btn_submit.setFont(MAIN_LABEL_FONT)
         self.btn_submit.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("../icons/accept.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.btn_submit.setIcon(icon1)
+        btn_submit_icon = QtGui.QIcon()
+        btn_submit_icon.addPixmap(QtGui.QPixmap("../icons/accept.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.btn_submit.setIcon(btn_submit_icon)
         self.btn_submit.setIconSize(QtCore.QSize(60, 60))
         self.btn_submit.setFlat(True)
-        self.btn_submit.setObjectName("btn_submit")
-        self.btn_layout_inner.addWidget(self.btn_submit)
-
-        self.btn_reject = QtWidgets.QPushButton(config_dialog)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.btn_reject.sizePolicy().hasHeightForWidth())
-        self.btn_reject.setSizePolicy(sizePolicy)
         self.btn_reject.setFont(MAIN_LABEL_FONT)
         self.btn_reject.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap("../icons/reject.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.btn_reject.setIcon(icon2)
+        btn_reject_icon = QtGui.QIcon()
+        btn_reject_icon.addPixmap(QtGui.QPixmap("../icons/reject.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.btn_reject.setIcon(btn_reject_icon)
         self.btn_reject.setIconSize(QtCore.QSize(60, 60))
         self.btn_reject.setFlat(True)
-        self.btn_reject.setObjectName("btn_reject")
-        self.btn_layout_inner.addWidget(self.btn_reject)
-        self.btn_layout_inner.setStretch(0, 50)
-        self.btn_layout_inner.setStretch(1, 50)
-        self.btn_layout_outer.addLayout(self.btn_layout_inner)
-        self.btn_layout_outer.setStretch(0, 40)
 
-        self.control_layout.addLayout(self.btn_layout_outer)
+        btn_layout.addWidget(self.btn_submit)
+        btn_layout.addWidget(self.btn_reject)
+        btn_layout.setStretch(0, 50)
+        btn_layout.setStretch(1, 50)
+        self.control_layout.addLayout(btn_layout)
         self.control_layout.setStretch(0, 75)
         self.control_layout.setStretch(1, 25)
-        self.horizontalLayout_3.addLayout(self.control_layout)
-        self.horizontalLayout_3.setStretch(0, 60)
-        self.horizontalLayout_3.setStretch(1, 40)
 
-        self.retranslateUi(config_dialog)
-        QtCore.QMetaObject.connectSlotsByName(config_dialog)
+    def _build_parameter_box(self):
+        """
 
-    def retranslateUi(self, config_dialog):
+        """
+
+        # Size policies
+        parameter_box_size_policy = SIZE_POLICY_EXPANDING
+        parameter_box_size_policy.setHorizontalStretch(0)
+        parameter_box_size_policy.setVerticalStretch(0)
+        parameter_box_size_policy.setHeightForWidth(self.parameter_box.sizePolicy().hasHeightForWidth())
+        self.parameter_box.setSizePolicy(parameter_box_size_policy)
+        self.parameter_box.setFont(BOX_LABEL_FONT)
+
+        # Layouts
+        row_layouts = [QtWidgets.QHBoxLayout() for _ in range(len(DEFAULT_PARAMETER_LABELS))]
+
+        # Labels
+        for label, text in zip(self.parameter_labels, DEFAULT_PARAMETER_LABELS):
+            label.setFont(MAIN_LABEL_FONT)
+            label.setText(text[1])  # This position contain the parameter label
+
+        # Spinners
+        for spin in self.parameter_spinners:
+            spin.setEnabled(True)
+            spin.setFont(MAIN_LABEL_FONT)
+            spin.setDecimals(10)
+            spin.setSingleStep(0.1)
+            spin.setValue(1)
+
+        for index, row_layout in enumerate(row_layouts):
+            row_layout.addWidget(self.parameter_labels[index])
+            row_layout.addWidget(self.parameter_spinners[index])
+            row_layout.setStretch(0, 40)
+            row_layout.setStretch(0, 60)
+            self.control_layout.addLayout(row_layout)
+
+        self.control_layout.addWidget(self.parameter_box)
+
+    def _build_preview_box(self):
+        """
+
+        """
+
+        # Size policies
+        preview_box_size_policy = SIZE_POLICY_EXPANDING
+        preview_box_size_policy.setHorizontalStretch(0)
+        preview_box_size_policy.setVerticalStretch(0)
+        preview_box_size_policy.setHeightForWidth(self.chart_preview_box.sizePolicy().hasHeightForWidth())
+        self.chart_preview_box.setSizePolicy(preview_box_size_policy)
+        self.chart_preview_box.setFont(BOX_LABEL_FONT)
+
+        # Layouts
+        preview_box_layout = QtWidgets.QHBoxLayout(self.chart_preview_box)
+        preview_box_layout.setContentsMargins(10, 15, 10, 10)
+        preview_box_layout.setSpacing(5)
+
+        self.chart_preview.setRenderHint(QtGui.QPainter.Antialiasing)
+
+        preview_box_layout.addWidget(self.chart_preview)
+        self.dialog_layout.addWidget(self.chart_preview_box)
+
+    def retranslateUi(self, dialog):
         _translate = QtCore.QCoreApplication.translate
-        config_dialog.setWindowTitle(_translate("config_dialog", "Parametrization"))
-        self.chart_preview_box.setTitle(_translate("config_dialog", "PDF chart preview"))
-        self.parameters_box.setTitle(_translate("config_dialog", "Distribution parameters"))
-        self.radiobtn_1.setText(_translate("config_dialog", ONE_PARAMETER))
-        self.radiobtn_2.setText(_translate("config_dialog", TWO_PARAMETERS))
-        self.radiobtn_3.setText(_translate("config_dialog", THREE_PARAMETERS))
-        self.parameter_label_1.setText(_translate("config_dialog", FIRST_PARAMETER))
-        self.parameter_label_2.setText(_translate("config_dialog", SECOND_PARAMETER))
-        self.parameter_label_3.setText(_translate("config_dialog", THIRD_PARAMETER))
-        self.parameter_label_4.setText(_translate("config_dialog", FOURTH_PARAMETER))
+        dialog.setWindowTitle(_translate("dialog", "Parametrization"))
+        self.chart_preview_box.setTitle(_translate("dialog", "PDF chart preview"))
+        self.parameter_box.setTitle(_translate("dialog", "Distribution parameters"))

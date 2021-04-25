561d9a9f98449e22014d647fc0e2812332e176b5
Daniel Restrepo <daniel.restrepo@unillanos.edu.co>
2021-04-25 14:10:53 -0500

    Refactor new approach

--- src/dialogs.py
+++ src/dialogs.py
@@ -1,7 +1,10 @@
-from src.qt.dialog import UiConfigDialog, QtWidgets
+from PyQt5 import QtWidgets
+
+from src.qt.dialog import DialogTemplate
+# from src.charts import stats
 
 
-class ParametrizationDialog(UiConfigDialog, QtWidgets.QDialog):
+class ParametrizationDialog(QtWidgets.QDialog, DialogTemplate):
     """
     Class used for the construction of a dialog box that allows
     the parameterization of a probability distribution
@@ -9,18 +12,10 @@ class ParametrizationDialog(UiConfigDialog, QtWidgets.QDialog):
 
     def __init__(self, *args, **kwargs):
         super(ParametrizationDialog, self).__init__(*args)
-        self.setupUi(self)  # Build the GUI designed with Qt designer assistant
-
+        self.setup(self)                                        # Build the GUI designed with Qt designer assistant
         self.distribution = kwargs.get('distribution', None)
-        self.distribution_chart = PDFChart(
-            title=f'''
-                    Probability Density Function
-                    <center><small>{self.distribution.name} Distribution</small></center>
-            ''',
-            parameters=self.distribution.parameters
-        )
-        self.setWindowTitle(f'{self.distribution.name} Distribution')
-        self.hide_parameter_rows()
+        self.distribution_chart = self.distribution.pdf_chart()
+        # self.hide_parameter_rows()
         self.setup_dialog_components()
         self.plot_chart_preview()
 
@@ -32,40 +27,38 @@ class ParametrizationDialog(UiConfigDialog, QtWidgets.QDialog):
         # self.radiobtn_3.toggled.connect(self.__radiobtn_checked)
 
         # Spin signals connection
-        self.parameter_spbox_1.valueChanged.connect(self.plot_chart_preview)
-        self.parameter_spbox_2.valueChanged.connect(self.plot_chart_preview)
-        self.parameter_spbox_3.valueChanged.connect(self.plot_chart_preview)
-        self.parameter_spbox_4.valueChanged.connect(self.plot_chart_preview)
+        # for spinner in self.parameter_spinners:
+        #     spinner.valueChanged.connect(self.plot_chart_preview())
 
     def closeEvent(self, event):
         """
         Override of the closeEvent method to close the window
         """
+
         self.reject()
         event.accept()
 
     def hide_parameter_rows(self):
-        for label, spbox in zip(self.parameter_labels, self.spboxes):
+        for label, spinner in zip(self.parameter_labels, self.parameter_spinners):
             label.setVisible(False)
-            spbox.setVisible(False)
+            spinner.setVisible(False)
 
-    def hide_radiobtn_rows(self):
-        self.radiobtn_1.setVisible(False)
-        self.radiobtn_2.setVisible(False)
-        self.radiobtn_3.setVisible(False)
+    # def hide_radiobtn_rows(self):
+    #     self.radiobtn_1.setVisible(False)
+    #     self.radiobtn_2.setVisible(False)
+    #     self.radiobtn_3.setVisible(False)
 
     def update_distribution_parameters(self):
-        for spbox, parameter in zip(self.spboxes, self.distribution.parameters):
-            parameter.set_value(spbox.value())
+        for spinner, parameter in zip(self.parameter_spinners, self.distribution.parameters):
+            parameter.set_value(spinner.value())
 
     def plot_chart_preview(self):
         """
-        Method
+
         :return:
         """
         self.update_distribution_parameters()
-        self.distribution.plot_pdfchart(self.distribution_chart)
-        self.pdf_chartview.setChart(self.distribution_chart)
+        self.chart_preview.setChart(self.distribution_chart)
 
     def setup_dialog_components(self):
         """
@@ -75,16 +68,10 @@ class ParametrizationDialog(UiConfigDialog, QtWidgets.QDialog):
         for index, parameter in enumerate(self.distribution.parameters):
             self.parameter_labels[index].setText(f'{parameter.name} \nparameter:')
             self.parameter_labels[index].setVisible(True)
-            self.spboxes[index].setMinimum(parameter.range[0])
-            self.spboxes[index].setMaximum(parameter.range[1])
-            self.spboxes[index].setValue(parameter.value)
-            self.spboxes[index].setVisible(True)
-
-        if self.distribution.variant:
-            pass
-
-        else:
-            self.hide_radiobtn_rows()
+            # self.parameter_spinners[index].setMinimum(parameter.range[0])
+            # self.parameter_spinners[index].setMaximum(parameter.range[1])
+            self.parameter_spinners[index].setValue(parameter.value)
+            self.parameter_spinners[index].setVisible(True)
 
 
 class CloseWindowDialog(QtWidgets.QDialog):

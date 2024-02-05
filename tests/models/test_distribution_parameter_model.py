f900053aa24a867528db3525e01eea147dcea623
Daniel <danielrestrepo9725@gmail.com>
2024-02-05 11:45:36 -0500

    IMPROVE: More unit tests and validation enhancements

--- tests/models/test_distribution_parameter_model.py
+++ tests/models/test_distribution_parameter_model.py
@@ -27,8 +27,8 @@ class TestDistributionParameterModel(unittest.TestCase):
 
     def test_mutate_distribution_parameter_value(self):
         interval = (0, 0.5)
-        value = fake.pyfloat(min_value=interval[0], max_value=interval[1])
-        distribution_parameter = DistributionParameter(name=fake.word(), interval=interval, value=value)
+        valid_value = fake.pyfloat(min_value=interval[0], max_value=interval[1])
+        distribution_parameter = DistributionParameter(name=fake.word(), interval=interval, value=valid_value)
         with self.assertRaises(ValueError):
             distribution_parameter.name = fake.word()
 
@@ -36,13 +36,41 @@ class TestDistributionParameterModel(unittest.TestCase):
             distribution_parameter.value = fake.pyfloat(min_value=interval[1]+1)
 
         distribution_parameter.value = fake.pyfloat(min_value=interval[0], max_value=interval[1])
-        self.assertNotEqual(distribution_parameter.value, value)
+        self.assertNotEqual(distribution_parameter.value, valid_value)
 
     def test_create_distribution_parameter_with_boundless_interval(self):
         interval = (None, None)
         value = fake.pyfloat()
         distribution_parameter = DistributionParameter(name=self.random_name, interval=interval, value=value)
-        self.assertEqual(distribution_parameter.name, self.random_name)
         self.assertEqual(distribution_parameter.interval, interval)
         self.assertTrue(distribution_parameter.is_boundless)
         self.assertEqual(distribution_parameter.value, value)
+
+    def test_create_distribution_parameter_with_lower_bound_interval(self):
+        interval = (0, None)
+        valid_value = fake.pyfloat(min_value=interval[0])
+        distribution_parameter = DistributionParameter(name=self.random_name, interval=interval, value=valid_value)
+        self.assertEqual(distribution_parameter.interval, interval)
+        self.assertFalse(distribution_parameter.is_boundless)
+        self.assertEqual(distribution_parameter.value, valid_value)
+        with self.assertRaises(ValueError):
+            distribution_parameter.value = fake.pyfloat(max_value=interval[0]-1)
+
+    def test_create_distribution_parameter_with_upper_bound_interval(self):
+        interval = (None, 1)
+        valid_value = fake.pyfloat(max_value=interval[1])
+        distribution_parameter = DistributionParameter(name=self.random_name, interval=interval, value=valid_value)
+        self.assertEqual(distribution_parameter.interval, interval)
+        self.assertFalse(distribution_parameter.is_boundless)
+        self.assertEqual(distribution_parameter.value, valid_value)
+        with self.assertRaises(ValueError):
+            distribution_parameter.value = fake.pyfloat(min_value=interval[1]+1)
+
+    def test_create_distribution_parameter_with_invalid_interval(self):
+        interval = (1, 0)
+        with self.assertRaises(ValueError):
+            DistributionParameter(
+                name=self.random_name,
+                interval=interval,
+                value=0
+            )
--- tests/models/test_probability_distribution_model.py
+++ tests/models/test_probability_distribution_model.py
@@ -3,7 +3,12 @@ import unittest
 from faker import Faker
 
 from src.models.distribution import ProbabilityDistribution, ProbabilityDistributionCategory
-from src.constants.distributions import AVAILABLE_PROBABILITY_DISTRIBUTIONS, DISTRIBUTION_DESCRIPTION_DEFAULT
+from src.constants.distributions import (
+    AVAILABLE_PROBABILITY_DISTRIBUTIONS,
+    DISTRIBUTION_DESCRIPTION_DEFAULT,
+    BERNOULLI,
+    NORMAL
+)
 
 fake = Faker()
 
@@ -16,9 +21,17 @@ class TestProbabilityDistributionModel(unittest.TestCase):
         distribution = ProbabilityDistribution(name=name, category=category)
         self.assertEqual(distribution.name, name)
         self.assertEqual(distribution.category, category.value)
-        self.assertNotEqual(distribution.description, DISTRIBUTION_DESCRIPTION_DEFAULT)
+        self.assertEqual(distribution.description, DISTRIBUTION_DESCRIPTION_DEFAULT)
 
-    def test_mutate_probability_distribution(self):
+    def test_create_continuous_probability_distribution(self):
+        distribution = ProbabilityDistribution(name=NORMAL, category=ProbabilityDistributionCategory.CONTINUOUS)
+        self.assertTrue(distribution.is_continuous)
+
+    def test_create_discrete_probability_distribution(self):
+        distribution = ProbabilityDistribution(name=BERNOULLI, category=ProbabilityDistributionCategory.DISCRETE)
+        self.assertFalse(distribution.is_continuous)
+
+    def test_probability_distribution_immutability(self):
         name = fake.random_element(AVAILABLE_PROBABILITY_DISTRIBUTIONS)
         category = fake.enum(ProbabilityDistributionCategory)
         distribution = ProbabilityDistribution(name=name, category=category)
@@ -28,5 +41,13 @@ class TestProbabilityDistributionModel(unittest.TestCase):
         with self.assertRaises(ValueError):
             distribution.category = ProbabilityDistributionCategory.CONTINUOUS
 
+        with self.assertRaises(ValueError):
+            distribution.description = fake.sentence()
+
+        with self.assertRaises(ValueError):
+            distribution.parameters = []
+
         self.assertEqual(distribution.name, name)
         self.assertEqual(distribution.category, category.value)
+        self.assertEqual(distribution.description, DISTRIBUTION_DESCRIPTION_DEFAULT)
+        self.assertEqual(distribution.parameters, [])

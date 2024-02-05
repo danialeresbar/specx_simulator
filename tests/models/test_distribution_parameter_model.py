77add99e15866fd0f06ca89bd1fa5a3a7a41aaea
Daniel <danielrestrepo9725@gmail.com>
2024-02-05 11:13:52 -0500

    IMPROVE: More unit tests and validation enhancements

--- tests/models/test_distribution_parameter_model.py
+++ tests/models/test_distribution_parameter_model.py
@@ -2,30 +2,33 @@ import unittest
 
 from faker import Faker
 
-from src.models.distributions import DistributionParameter
+from src.models.distribution import DistributionParameter
 
 fake = Faker()
 
 
 class TestDistributionParameterModel(unittest.TestCase):
 
-    def test_create_distribution_parameter_with_default_interval(self):
-        name = fake.word()
-        interval = (0, 1)
-        value = fake.pyfloat(min_value=interval[0], max_value=interval[1])
-        distribution_parameter = DistributionParameter(name=name, interval=interval, value=value)
-        self.assertEqual(distribution_parameter.name, name)
-        self.assertEqual(distribution_parameter.interval, interval)
-        self.assertEqual(distribution_parameter.value, value)
+    def setUp(self):
+        self.random_name = fake.word()
+        self.default_interval = (0, 1)
 
-    def test_mutate_distribution_parameter(self):
-        interval = (0, 0.5)
-        value = fake.pyfloat(min_value=interval[0], max_value=interval[1])
+    def test_create_distribution_parameter_with_default_interval(self):
+        value = fake.pyfloat(min_value=self.default_interval[0], max_value=self.default_interval[1])
         distribution_parameter = DistributionParameter(
-            name=fake.word(),
-            interval=interval,
+            name=self.random_name,
+            interval=self.default_interval,
             value=value
         )
+        self.assertEqual(distribution_parameter.name, self.random_name)
+        self.assertEqual(distribution_parameter.interval, self.default_interval)
+        self.assertFalse(distribution_parameter.is_boundless)
+        self.assertEqual(distribution_parameter.value, value)
+
+    def test_mutate_distribution_parameter_value(self):
+        interval = (0, 0.5)
+        value = fake.pyfloat(min_value=interval[0], max_value=interval[1])
+        distribution_parameter = DistributionParameter(name=fake.word(), interval=interval, value=value)
         with self.assertRaises(ValueError):
             distribution_parameter.name = fake.word()
 
@@ -34,3 +37,12 @@ class TestDistributionParameterModel(unittest.TestCase):
 
         distribution_parameter.value = fake.pyfloat(min_value=interval[0], max_value=interval[1])
         self.assertNotEqual(distribution_parameter.value, value)
+
+    def test_create_distribution_parameter_with_boundless_interval(self):
+        interval = (None, None)
+        value = fake.pyfloat()
+        distribution_parameter = DistributionParameter(name=self.random_name, interval=interval, value=value)
+        self.assertEqual(distribution_parameter.name, self.random_name)
+        self.assertEqual(distribution_parameter.interval, interval)
+        self.assertTrue(distribution_parameter.is_boundless)
+        self.assertEqual(distribution_parameter.value, value)

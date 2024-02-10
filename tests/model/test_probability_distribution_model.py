2e9b565035f27d1259de5663b0499875c1801d02
Daniel <danielrestrepo9725@gmail.com>
2024-02-10 15:17:01 -0500

    ADD: More unit tests for the models package

--- tests/model/test_probability_distribution_model.py
+++ tests/model/test_probability_distribution_model.py
@@ -1,6 +1,7 @@
 import unittest
 
 from faker import Faker
+from pathlib import Path
 
 from src.models.distribution import ProbabilityDistribution, ProbabilityDistributionCategory
 from src.constants.distributions import (
@@ -9,6 +10,9 @@ from src.constants.distributions import (
     BERNOULLI,
     NORMAL
 )
+from src.constants.paths import TESTS_PATH
+from src.tools.files import load_json, export_json
+from tests.utils import delete_tmp_file
 
 fake = Faker()
 
@@ -51,3 +55,18 @@ class TestProbabilityDistributionModel(unittest.TestCase):
         self.assertEqual(distribution.category, category.value)
         self.assertEqual(distribution.description, DISTRIBUTION_DESCRIPTION_DEFAULT)
         self.assertEqual(distribution.parameters, [])
+
+    def test_dump_probability_distribution_to_json(self):
+        name = fake.random_element(AVAILABLE_PROBABILITY_DISTRIBUTIONS)
+        category = fake.enum(ProbabilityDistributionCategory)
+        distribution = ProbabilityDistribution(name=name, category=category)
+        filepath = Path(f'{TESTS_PATH}/fixtures/tmp.json')
+        export_json(filepath=str(filepath), data=distribution.model_dump())
+        self.assertTrue(filepath.exists())
+        delete_tmp_file(str(filepath))
+
+    def test_load_probability_distributions_from_json_file(self):
+        json_data = load_json(f'{TESTS_PATH}/fixtures/probability_distributions.json')
+        distributions = [ProbabilityDistribution(**dumped_object) for dumped_object in json_data]
+        self.assertTrue(all(isinstance(distribution, ProbabilityDistribution) for distribution in distributions))
+        self.assertEqual(len(distributions), len(json_data))
